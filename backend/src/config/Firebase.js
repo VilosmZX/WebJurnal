8e7708265153d632797f5ec349e28445095e5a66
Joshua Ravael <jravaellnew@gmail.com>
2024-03-17 08:24:37 +0700

    Adding firebase for image and video

---
+++ backend/src/config/Firebase.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getStorage } from 'firebase/storage';
+
+const firebaseConfig = {
+  apiKey: process.env.apiKey,
+  authDomain: process.env.authDomain,
+  projectId: process.env.projectId,
+  storageBucket: process.env.storageBucket,
+  messagingSenderId: process.env.messagingSenderId,
+  appId: process.env.appId,
+  measurementId: process.env.measurementId
+};
+
+
+const firebaseApp = initializeApp(firebaseConfig);
+export const storage = getStorage(firebaseApp);
